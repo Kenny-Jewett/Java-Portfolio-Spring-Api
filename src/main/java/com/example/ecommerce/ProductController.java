35a216483d06ec599c44f1253228e67e88dba081
Kenny Jewett <kennethrjewett@gmail.com>
2023-07-12 10:20:08 -0500

    Added two new endpoints. A basic delete, and a getByName endpoint.

--- src/main/java/com/example/ecommerce/ProductController.java
+++ src/main/java/com/example/ecommerce/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    @PostMapping("/products/add")
+    @PostMapping("/products")
     public Product addProduct(@RequestBody Product product) {
         productRepository.save(product);
         return product;
@@ -26,6 +27,24 @@ public class ProductController {
     @GetMapping("/products/{name}")
         public List<Product> getProductByName(@PathVariable String name) {
         return productRepository.getProductByName(name);
+    }
+
+    @GetMapping("/products/model/{model}")
+    public List<Product> getProductByModel(@PathVariable String model) {
+        return productRepository.getProductByModel(model);
+    }
+
+    @Transactional
+    @DeleteMapping("products/{id}")
+    public String deleteGreetingById(@PathVariable long id) {
+        if(productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            return "Product Deleted";
+        } else {
+            return "Product not found.";
+        }
+
+
     }
 
 
